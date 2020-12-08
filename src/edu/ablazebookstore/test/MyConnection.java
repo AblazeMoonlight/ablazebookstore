189bd265d96d9c057e5d2179ac835dbdd58714a0
elyes <elyes.jarroudi@epsrit.tn>
2020-12-08 17:30:45 +0100

    package change

---
+++ src/edu/ablazebookstore/test/MyConnection.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.ablazebookstore.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author House
+ */
+public class MyConnection {
+    
+    public String url="jdbc:mysql://sql7.freesqldatabase.com:3306/sql7380413";
+    public String login="sql7380413";
+    public String pwd="6n4ejtqsQk";
+    Connection cnx;
+    public static MyConnection instance;
+
+    private MyConnection() {
+        try {
+            cnx = DriverManager.getConnection(url, login, pwd);
+            System.out.println("Connexion établie!");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    
+    }
+    
+    public Connection getCnx(){
+        return cnx;
+    }
+    public static MyConnection getInstance(){
+        
+            if(instance== null){
+                instance = new MyConnection();
+            }
+        return instance ;
+    }
+    
+    
+}
