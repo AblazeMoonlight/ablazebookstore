6df14e5824e7332a4ce54c3477d91f9d12441dc1
elyes <elyes.jarroudi@epsrit.tn>
2020-12-08 17:22:24 +0100

    package change

--- src/edu/ablazebookstore/gui/BookController.java
+++ src/edu/ablazebookstore/gui/BookController.java
@@ -21,6 +21,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.Alert;
 
 /**
  * FXML Controller class
@@ -54,13 +55,36 @@ public class BookController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+      
+    private boolean validatefields() {
 
+        if( tftitle.getText().isEmpty()||
+            txauthor.getText().isEmpty()||
+            txisbn.getText().isEmpty()||
+            txpublisher.getText().isEmpty()||
+            txcover.getText().isEmpty()||
+            txprice.getText().isEmpty() || 
+                txreleasedate.getEditor().getText().isEmpty())
+        {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("WARNING");
+            alert.setHeaderText(null);
+            alert.setContentText("make sure no fields are empty");
+            alert.showAndWait();
+            return false;
+            
+        }
+           return true;     
+    }
     @FXML
     private void addbook(ActionEvent event) {
 //        try {
             String title,author,publisher,isbn,cover;
             float price ;
+            
             Date  releasedate;
+            if(validatefields())
+            {
             title=tftitle.getText();
             author=txauthor.getText();
             isbn=txisbn.getText();
@@ -72,6 +96,7 @@ public class BookController implements Initializable {
             BookCrud pc = new BookCrud();
             Book p1 = new Book(price, title, author, isbn,publisher,  releasedate,cover);
             pc.addBook(p1);
+            }
 //            System.out.println("******redirection******");
 //            FXMLLoader loader = new FXMLLoader(getClass().getResource("PesonDetails.fxml"));
 //            Parent root2 = loader.load();
@@ -83,6 +108,6 @@ public class BookController implements Initializable {
 //            System.out.println(ex.getMessage());        }
 //                
     }
-
+  
 
 }
